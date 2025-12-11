5e28d2e75261ff60fb6fbee244d0a040fd7add37
ratulhasanruhan <ratulhasan1644@gmail.com>
2025-12-12 03:33:45 +0600

    minor changes

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 import random from 'random';
 import simpleGit from 'simple-git';
 import jsonfile from 'jsonfile';
-import momont from 'moment';
+import moment from 'moment';
 
 const FILE_PATH = './dummy.json';
 
 
 const makeCommit = (n) => {
-	if(n===0) return simpleGit().push();
-	const x = random.int(0, 54);
-	const y = random.int(0, 6);
-	const p = random.int(1, 7);
-	const DATE = momont().subtract(p, 'y').add(1, 'd').add(x,'w').add(y, 'd').format();
+		if(n===0) return simpleGit().push();
 
-	const data = {
-	  date: DATE,
-	};
+		// Random date strictly within 2025
+		const start = moment('2025-01-01T00:00:00Z');
+		const end = moment('2025-12-31T23:59:59Z');
+		const totalDays = end.diff(start, 'days'); // 364
+		const dayOffset = random.int(0, totalDays);
+		const hour = random.int(0, 23);
+		const minute = random.int(0, 59);
+		const second = random.int(0, 59);
+		const DATE = start.clone().add(dayOffset, 'days').hour(hour).minute(minute).second(second).utc().format();
 
-	console.log(`commiting on ${DATE}`);
-	
-	jsonfile.writeFile(FILE_PATH, data, ()=>{
-		simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE});
-		makeCommit.bind(this, --n)();
-	});
-	
+		const data = {
+			date: DATE,
+		};
+
+		console.log(`commiting on ${DATE}`);
+    
+		jsonfile.writeFile(FILE_PATH, data, ()=>{
+				simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE});
+				makeCommit.bind(this, --n)();
+		});
+    
 };
 
-makeCommit(10000);
+makeCommit(1000);
 
 
